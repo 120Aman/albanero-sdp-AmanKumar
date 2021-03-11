9fd6b1889b94059f5abccd0f558e1e6fc2a949b4
Aman Kumar <aman17520kumar@gmail.com>
2021-03-11 23:03:23 +0530

    Add files via upload

---
+++ hashset.java
@@ -0,0 +1,16 @@
+package setinterface;
+import java.util.*;  
+public class hashset {
+
+	public static void main(String args[]){   
+	HashSet<String> set=new HashSet<String>();  
+	set.add("Ravi");  
+	set.add("Vijay");  
+	set.add("Ravi");  
+	set.add("Ajay");  
+	Iterator<String> itr=set.iterator();  
+	while(itr.hasNext()){  
+	System.out.println(itr.next());  
+	}  
+	}  
+	}  
